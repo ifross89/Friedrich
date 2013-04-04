d09b0d525387274e0094e92c705d2608bcb4f248
James Aley <james.aley@gmail.com>
2013-04-04 14:16:13 +0100

    Minor tidying.

--- resources/public/js/dash.js
+++ resources/public/js/dash.js
@@ -1,11 +1,19 @@
+/**
+ * Dashboard State.
+ */
 dash = {
+    // Riemann server base URL for WebSockets API
+    baseURL: "ws://localhost:5556",
+
+    // Events received from Riemann.
     eventBuffer: []
 };
 
+/**
+ * Call Riemann's query API, returning open WebSocket object.'
+ */
 dash.openQuerySocket = function(query, message_fn) {
-    var uri = "ws://"
-        + window.location.hostname
-        + ":5556"
+    var uri = this.baseURL
         + "/index"
         + "?subscribe=true"
         + "&query=" + encodeURIComponent(query);
@@ -15,36 +23,11 @@ dash.openQuerySocket = function(query, message_fn) {
     return s;
 }
 
-dash.testQuery = function() {
-    dash.openQuerySocket("tagged \"foo\"", function(m) {
-        var data = JSON.parse(m.data);
-
-        if (data.service == "cpu") {
-            dash.eventBuffer.push({metric: data.metric,
-                                   time:   +Date.parse(data.time)});
-        }
-    });
-}
-
-dash.randomMetric = function(context) {
-    return context.metric(function(start, stop, step, callback) {
-
-        var values = [];
-
-        // convert start & stop to milliseconds
-        start = +start;
-        stop = +stop;
-
-        while (start < stop) {
-            start += step;
-            values.push(Math.random());
-        }
-
-        callback(null, values);
-    });
-}
-
-dash.cpuMetric = function(context) {
+/**
+ * Cubism "metric" function to bucketize events in the buffer
+ * and format them for rendering in the graph.
+ */
+function cpuMetric(context) {
     return context.metric(function(start, stop, step, callback) {
         values = [];
         start = +start;
@@ -88,7 +71,7 @@ dash.makeChart = function() {
                          .height(150)
                          .extent([0, 0.25]);
 
-    var met = dash.cpuMetric(context);
+    var met = cpuMetric(context);
     horizon.metric(met);
 
     d3.select("#time-series-container").selectAll(".axis")
@@ -110,6 +93,37 @@ dash.makeChart = function() {
 }
 
 dash.onLoad = function () {
-    dash.testQuery();
-    dash.makeChart();
+    this.testQuery();
+    this.makeChart();
+}
+
+
+// Test crap
+dash.testQuery = function() {
+    this.openQuerySocket("tagged \"foo\"", function(m) {
+        var data = JSON.parse(m.data);
+
+        if (data.service == "cpu") {
+            dash.eventBuffer.push({metric: data.metric,
+                                   time:   +Date.parse(data.time)});
+        }
+    });
+}
+
+dash.randomMetric = function(context) {
+    return context.metric(function(start, stop, step, callback) {
+
+        var values = [];
+
+        // convert start & stop to milliseconds
+        start = +start;
+        stop = +stop;
+
+        while (start < stop) {
+            start += step;
+            values.push(Math.random());
+        }
+
+        callback(null, values);
+    });
 }
