aa37ba10c5b9b9924e4c5779ee1a34d4f68b8667
James Aley <james.aley@gmail.com>
2013-04-04 15:45:58 +0100

    Making the dashboard plot all hosts/services coming back from the server.

--- resources/public/js/dash.js
+++ resources/public/js/dash.js
@@ -5,48 +5,99 @@ dash = {
     // Riemann server base URL for WebSockets API
     baseURL: "ws://localhost:5556",
 
+    // Query state
+    query: "tagged \"foo\"",
+    socket: null,
+
     // Events received from Riemann.
-    eventBuffer: []
+    events: {}
 };
 
+//
+// WebSocket handler functions.
+//
+
+function socketOnMessage(msg) {
+    if(! msg.data) {
+        console.log("Ignoring bad message from WebSocket: " + msg);
+    }
+
+    var event = JSON.parse(msg.data);
+    dash.addEvent(event);
+}
+
+function socketOnError(err) {
+    console.log("WebSocket Error: " + err);
+}
+
+function socketOnOpen() {
+    console.log("Websocket opened.");
+}
+
+function socketOnClose() {
+    console.log("WebSocket closed.");
+}
+
 /**
- * Call Riemann's query API, returning open WebSocket object.'
+ * Adds event e to the events log, indexed by host then service.
  */
-dash.openQuerySocket = function(query, message_fn) {
+dash.addEvent = function(e) {
+    if(! this.events[e.host]) {
+        this.events[e.host] = {};
+    }
+
+    if(! this.events[e.host][e.service]) {
+        this.events[e.host][e.service] = [];
+    }
+
+    // Convert time to milliseconds when adding to buffers.
+    this.events[e.host][e.service].push({time:   +Date.parse(e.time),
+                                         metric: e.metric});
+}
+
+/**
+ * Call Riemann's query API, setting this.socket to the
+ * open WebSocket object.
+ */
+dash.openQuery = function() {
     var uri = this.baseURL
         + "/index"
         + "?subscribe=true"
-        + "&query=" + encodeURIComponent(query);
+        + "&query=" + encodeURIComponent(this.query);
 
-    var s = new WebSocket(uri);
-    s.onmessage = message_fn;
-    return s;
+    this.socket = new WebSocket(uri);
+    this.socket.onmessage = socketOnMessage;
+    this.socket.onerror = socketOnError;
+    this.socket.onopen = socketOnOpen;
+    this.socket.onclose = socketOnClose;
 }
 
 /**
  * Cubism "metric" function to bucketize events in the buffer
  * and format them for rendering in the graph.
  */
-function cpuMetric(context) {
+function eventsMetric(host, service, context) {
     return context.metric(function(start, stop, step, callback) {
+        var eventBuffer = dash.events[host][service];
+
         values = [];
         start = +start;
         stop = +stop;
 
         // Delete old events
-        while(dash.eventBuffer.length > 0
-              && dash.eventBuffer[0] < start) {
-            dash.eventBuffer.shift();
+        while(eventBuffer.length > 0
+              && eventBuffer[0] < start) {
+            eventBuffer.shift();
         }
 
         var eventIndex = 0;
         for(; start <= stop; start += step) {
-            var nextMetric = 0;
+            var nextMetric = NaN;
             var bucket = [];
 
-            for(; eventIndex < dash.eventBuffer.length
-               && dash.eventBuffer[eventIndex].time < start + step; ++eventIndex) {
-                bucket.push(dash.eventBuffer[eventIndex].metric);
+            for(; eventIndex < eventBuffer.length
+               && eventBuffer[eventIndex].time < start + step; ++eventIndex) {
+                bucket.push(eventBuffer[eventIndex].metric);
             }
 
             if(bucket.length > 0) {
@@ -58,72 +109,55 @@ function cpuMetric(context) {
         }
 
         callback(null, values);
-    });
+    }, host + ": " + service);
 }
 
-var data = ["CPU", "Blah"];
-dash.makeChart = function() {
-    var context = cubism.context()
-                        .size(900)
-                        .step(1000);
+/**
+ * Return a function to be called at some interval, which checks for as-yet
+ * uncharted hosts/services and adds them to the dashboard. Note that no
+ * graphs appear at all until this has been called at least once while there
+ * was data in dash.events.
+ */
+function chartsUpdater(context) {
+    return function() {
+        var horizon = context.horizon()
+            .height(30)
+            .extent([0, 0.25]);
 
-    var horizon = context.horizon()
-                         .height(150)
-                         .extent([0, 0.25]);
+        var metrics = [];
+        for(host in dash.events) {
+            for(service in dash.events[host]) {
+                metrics.push(eventsMetric(host, service, context))
+            }
+        }
 
-    var met = cpuMetric(context);
-    horizon.metric(met);
+        d3.select("#time-series-container").selectAll(".axis")
+            .data(["top", "bottom"])
+            .enter().append("div")
+            .attr("class", function(d) {return d + " axis"})
+            .each(function(d) {d3.select(this).call(context.axis().ticks(12).orient(d)); });
 
-    d3.select("#time-series-container").selectAll(".axis")
-        .data(["top", "bottom"])
-        .enter().append("div")
-        .attr("class", function(d) {return d + " axis"})
-        .each(function(d) {d3.select(this).call(context.axis().ticks(12).orient(d)); });
+        d3.select("#time-series-container").append("div")
+            .attr("class", "rule")
+            .call(context.rule());
 
-    d3.select("#time-series-container").append("div")
-        .attr("class", "rule")
-        .call(context.rule());
-
-    d3.select("#time-series-container")
-        .selectAll(".horizon")
-        .data(data)
-        .enter().append("div")
-                .attr("class", "horizon")
-                .call(horizon);
+        d3.select("#time-series-container")
+            .selectAll(".horizon")
+            .data(metrics)
+            .enter().append("div")
+            .attr("class", "horizon")
+            .call(horizon);
+    }
 }
 
+/**
+ * Call from body onLoad, after scripts loaded.
+ */
 dash.onLoad = function () {
-    this.testQuery();
-    this.makeChart();
-}
+    var context = cubism.context()
+        .size(900)
+        .step(1000);
 
-
-// Test crap
-dash.testQuery = function() {
-    this.openQuerySocket("tagged \"foo\"", function(m) {
-        var data = JSON.parse(m.data);
-
-        if (data.service == "cpu") {
-            dash.eventBuffer.push({metric: data.metric,
-                                   time:   +Date.parse(data.time)});
-        }
-    });
-}
-
-dash.randomMetric = function(context) {
-    return context.metric(function(start, stop, step, callback) {
-
-        var values = [];
-
-        // convert start & stop to milliseconds
-        start = +start;
-        stop = +stop;
-
-        while (start < stop) {
-            start += step;
-            values.push(Math.random());
-        }
-
-        callback(null, values);
-    });
+    this.openQuery();
+    window.setInterval(chartsUpdater(context), 5000);
 }
